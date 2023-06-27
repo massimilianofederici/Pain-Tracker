6bcace004b397d92337e085c54386a706306fa4f
massimilianofederici <12109500+massimilianofederici@users.noreply.github.com>
2023-06-27 19:37:00 +0100

    Additional movement and slider changes

--- Pain Tracker/Views/DetailEditView.swift
+++ Pain Tracker/Views/DetailEditView.swift
@@ -10,7 +10,6 @@ import SwiftUI
 struct DetailEditView: View {
     @Binding var entry: Entry
     @State private var intensity = 0.0
-    @State private var isEditing = false
     
     var body: some View {
         Form {
@@ -21,47 +20,17 @@ struct DetailEditView: View {
                         in: Double(PainLevel.noPain.rawValue)...Double(PainLevel.unableToMove.rawValue),
                         step: 1,
                         onEditingChanged: { editing in
-                            isEditing = editing
                             entry.intensity = Int(intensity)
-                        }
+                        },
+                        minimumValueLabel: Text("\(PainLevel.noPain.rawValue)"),
+                        maximumValueLabel: Text("\(PainLevel.unableToMove.rawValue)"), label: { }
                     )
-                    Text("\(intensity)").foregroundColor(isEditing ? .red : .blue)
+                    Text("\(PainLevel(rawValue: Int(intensity))!.name)")
                 }
                 TextField("Location", text: $entry.location)
-                TextField("Notes", text: $entry.notes)
-                //                HStack {
-                //                    Slider(value: $scrum.lengthInMinutesAsDouble, in: 5...30, step: 1) {
-                //                        Text("Length")
-                //                    }
-                //                    .accessibilityValue("\(scrum.lengthInMinutes) minutes")
-                //                    Spacer()
-                //                    Text("\(scrum.lengthInMinutes) minutes")
-                //                        .accessibilityHidden(true)
-                //                }
-                //                ThemePicker(selection: $scrum.theme)
+                TextField("Movement", text: $entry.movement)
+                TextField("Notes", text: $entry.notes).lineLimit(2)
             }
-            //            Section(header: Text("Attendees")) {
-            //                ForEach(scrum.attendees) { attendee in
-            //                    Text(attendee.name)
-            //                }
-            //                .onDelete { indices in
-            //                    scrum.attendees.remove(atOffsets: indices)
-            //                }
-            //                HStack {
-            //                    TextField("New Attendee", text: $newAttendeeName)
-            //                    Button(action: {
-            //                        withAnimation {
-            //                            let attendee = DailyScrum.Attendee(name: newAttendeeName)
-            //                            scrum.attendees.append(attendee)
-            //                            newAttendeeName = ""
-            //                        }
-            //                    }) {
-            //                        Image(systemName: "plus.circle.fill")
-            //                            .accessibilityLabel("Add attendee")
-            //                    }
-            //                    .disabled(newAttendeeName.isEmpty)
-            //                }
-            //            }
         }
     }
 }
